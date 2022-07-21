4ccf488b99298a013afb63a9dfbc64e1bcb63d2d
beg beg <sweet.bebe67@gmail.com>
2022-07-21 20:27:10 +0300

    modify users

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -17,24 +17,18 @@ const dayjs_1 = __importDefault(require("dayjs"));
 const bcrypt = require("bcrypt");
 var router = express_1.default.Router();
 var database = require("./database");
-/* GET users listing. */
 router.post('/register', function (req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         const firstName = req.body.firstName;
-        const lasttName = req.body.lastName;
+        const lastName = req.body.lastName;
         const userName = req.body.userName;
-        const phoneNum = req.body.phoneNum;
+        const phoneNum = req.body.phoneNumber; //req.body.phoneNum; smh 
         const password = req.body.password;
         const hashedPass = bcrypt.hashSync(password, 10);
         const created_on = (0, dayjs_1.default)().format();
-        // try{
-        //   console.log(created_on)
-        // }catch(err){
-        //   console.log(err)
-        // }
         console.log(req.body, hashedPass, created_on);
         var q = `INSERT INTO users (firstname, lastname, username, password, created_on, phone_num)
-    VALUES ('${firstName}', '${lasttName}', '${userName}', '${hashedPass}', '${created_on}', '${phoneNum}');`;
+    VALUES ('${firstName}', '${lastName}', '${userName}', '${hashedPass}', '${created_on}', '${phoneNum}');`;
         try {
             yield database.query(q);
             let result = yield logIn(req.body);
@@ -74,6 +68,39 @@ function logIn(body) {
         }
     });
 }
+router.post('/modify/:id', function (req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const id = req.params.id;
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const userName = req.body.userName;
+        const phoneNum = req.body.phoneNumber;
+        const pass = req.body.password;
+        const hashedPass = bcrypt.hashSync(pass, 10);
+        const query = `UPDATE users SET firstname = '${firstName}',  lastname ='${lastName}',
+       username ='${userName}', password ='${hashedPass}', phone_num = '${phoneNum}' WHERE id =${id};`;
+        try {
+            yield database.query(query);
+            try {
+                const info = `SELECT * FROM users WHERE id = ${id}`;
+                const { rows } = yield database.query(info);
+                if (rows[0]) {
+                    console.log("modify user", rows[0]);
+                    return res.json(rows[0]);
+                }
+                else {
+                    return res.status(400).json({ "error": "something" });
+                }
+            }
+            catch (err) {
+                console.log("DATABASE EERROR", err);
+            }
+        }
+        catch (error) {
+            console.log("DATABASE EERROR", error);
+        }
+    });
+});
 router.post('/login', function (req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
