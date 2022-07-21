e0df7e262c8f18f8287ef608b259f653fd074c48
beg beg <sweet.bebe67@gmail.com>
2022-07-21 20:24:09 +0300

    add modify to users

--- routes/users.ts
+++ routes/users.ts
@@ -7,16 +7,16 @@ var database =  require("./database")
 
 router.post('/register', async function(req, res, next) {
   const firstName : string = req.body.firstName;
-  const lasttName : string = req.body.lastName;
+  const lastName : string = req.body.lastName;
   const userName : string = req.body.userName;
-  const phoneNum : string = req.body.phoneNumber; //req.body.phoneNum; smh sorryy
+  const phoneNum : string = req.body.phoneNumber; //req.body.phoneNum; smh 
   const password : string = req.body.password;
   const hashedPass : string = bcrypt.hashSync(password,10);
   const created_on : any = dayjs().format()
 
   console.log(req.body,hashedPass,created_on)
   var q : string = `INSERT INTO users (firstname, lastname, username, password, created_on, phone_num)
-    VALUES ('${firstName}', '${lasttName}', '${userName}', '${hashedPass}', '${created_on}', '${phoneNum}');`
+    VALUES ('${firstName}', '${lastName}', '${userName}', '${hashedPass}', '${created_on}', '${phoneNum}');`
 
   try{
     await database.query(q)
@@ -40,11 +40,9 @@ async function logIn(body:any) {
   rows = [];
   try {
     var { rows, rowCount } = await database.query(q);
-
     if (rowCount == 0) {
       return null;
     }
-
     var verified = bcrypt.compareSync(body.password, rows[0]["password"]);
   } catch (error) {
     console.log(error);
@@ -56,6 +54,39 @@ async function logIn(body:any) {
   }
 }
 
+router.post('/modify/:id', async function(req, res, next) {
+  const id = req.params.id;
+  const firstName : string = req.body.firstName;
+  const lastName : string = req.body.lastName;
+  const userName : string = req.body.userName;
+  const phoneNum : string = req.body.phoneNumber;
+  const pass : string = req.body.password;
+  const hashedPass : string = bcrypt.hashSync(pass,10);
+
+
+
+  const query : string = `UPDATE users SET firstname = '${firstName}',  lastname ='${lastName}',
+       username ='${userName}', password ='${hashedPass}', phone_num = '${phoneNum}' WHERE id =${id};`
+
+  try {
+       await database.query(query);
+     try{
+      const info = `SELECT * FROM users WHERE id = ${id}`
+      const {rows} = await database.query(info)
+      if(rows[0]){
+        console.log("modify user",rows[0])
+        return res.json(rows[0])
+      }else{
+        return res.status(400).json({"error": "something" });
+       }
+     }catch(err){
+      console.log("DATABASE EERROR", err)
+     }
+  } catch (error) {
+    console.log("DATABASE EERROR", error)
+  }
+  
+})
 
 
 router.post('/login', async function(req, res, next) {
