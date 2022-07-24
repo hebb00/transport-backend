5d7f8090cf55aa53a09603580fba90d7834c5598
beg beg <sweet.bebe67@gmail.com>
2022-07-24 20:37:49 +0300

    new routs

--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
 var createError = require("http-errors");
 import express, {Application, Request, Response,NextFunction} from "express";
-// var express = require("express");
 // var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
-
+var session = require("express-session");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var driversRouter = require("./routes/drivers");
+var clientsRouter = require("./routes/clients");
+var vehiclesRouter = require("./routes/vehicles");
+var reservationsRouter = require("./routes/reservations");
+
 
 var app:Application = express();
 
@@ -20,10 +24,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 // app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/drivers", driversRouter);
+app.use("/clients",clientsRouter);
+app.use("/vehicles", vehiclesRouter);
+app.use("/reservations", reservationsRouter);
+
+
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
